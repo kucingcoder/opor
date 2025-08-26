99d8da96c77ef41e042bcc24f88391a8879a4a5e
M. Ibrahim Hanif <137396727+kucingcoder@users.noreply.github.com>
2025-08-27 00:36:11 +0700

    Remove unused User model import and add UserSeeder for database seeding

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'username'   => 'admin',
+            'password'   => Hash::make('admin'),
+            'full_name'  => 'John Doe',
+            'profession' => 'Senior Web Developer',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante.',
+            'email'      => 'johndoe@gmail.com',
+            'phone'      => '08123456789',
+            'address'    => 'Tegal, Indonesia',
+            'photo'      => '202cb962ac59075b964b07152d234b70',
+        ]);
+    }
+}
